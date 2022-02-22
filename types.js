0086d5e73bd53b976b8ce210bc509c83b9eb1413
zaneShen <635879675@qq.com>
2022-02-22 16:14:37 +0800

    添加jstype

--- types.js
+++ types.js
@@ -5,73 +5,88 @@ module.exports = {
   bytes: {
     type: 'string',
     primitive: 'bytes',
+    jstype: 'string',
   },
   string: {
     type: 'string',
     primitive: 'string',
+    jstype: 'string',
   },
   bool: {
     type: 'boolean',
-    primitive: 'boolean',
+    primitive: 'bool',
+    jstype: 'boolean',
   },
   int32: {
     type: 'integer',
     minimum: -0x80000000,
     maximum: 0x7fffffff,
     primitive: 'int32',
+    jstype: 'number',
   },
   sint32: {
     type: 'integer',
     minimum: -0x80000000,
     maximum: 0x7fffffff,
     primitive: 'sint32',
+    jstype: 'number',
   },
   uint32: {
     type: 'integer',
     minimum: 0,
     maximum: 0xffffffff,
     primitive: 'uint32',
+    jstype: 'number',
   },
   int64: {
     type: 'integer',
     minimum: MIN_SAFE_INTEGER,
     maximum: MAX_SAFE_INTEGER,
     primitive: 'int64',
+    jstype: 'number',
   },
   sint64: {
     type: 'integer',
     minimum: MIN_SAFE_INTEGER,
     maximum: MAX_SAFE_INTEGER,
     primitive: 'sint64',
+    jstype: 'number',
   },
   uint64: {
     type: 'integer',
     minimum: 0,
     maximum: MAX_SAFE_INTEGER,
     primitive: 'uint64',
+    jstype: 'number',
   },
   fixed32: {
     type: 'number',
     primitive: 'fixed32',
+    jstype: 'number',
   },
   fixed64: {
     type: 'number',
     primitive: 'fixed64',
+    jstype: 'number',
   },
   sfixed32: {
     type: 'number',
     primitive: 'sfixed32',
+    jstype: 'number',
   },
   sfixed64: {
     type: 'number',
     primitive: 'sfixed64',
+    jstype: 'number',
   },
   float: {
     type: 'number',
     primitive: 'float',
+    jstype: 'number',
   },
   double: {
     type: 'number',
     primitive: 'double',
+    jstype: 'number',
   }
 };
